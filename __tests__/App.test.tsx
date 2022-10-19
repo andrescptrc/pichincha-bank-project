fc19266285535a89cb44ea460a4c1a76349b6eab
Andrés Carreño <andrebrayan74@gmail.com>
2022-10-18 21:28:19 -0500

    chore: first commit

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../src/App";
+
+describe("App", () => {
+  it("should work as expected", () => {
+    render(<App />);
+    const h1Element = screen.getByText(/vite react/i);
+    expect(h1Element).toBeInTheDocument();
+  });
+});
---
+++ config/jest/cssTransform.cjs
@@ -0,0 +1,13 @@
+// config/jest/cssTransform.js
+"use strict";
+
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process() {
+    return {
+      code: "module.exports = {};",
+    };
+  },
+};
